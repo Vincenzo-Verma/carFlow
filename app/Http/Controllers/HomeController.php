cc051d79b985bf74260dbbd0df1ab1e8a13d1ab4
Esoteric Dev <utk.rs.verma01@gmail.com>
2025-04-02 22:01:34 +0530

    Formatted Text

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
+use App\Models\Car;
 
 class HomeController extends Controller
 {
-    function index() {
-    //    return view('home.index', [ 'name' => 'John Doe', 'age' => 25 ]); 
-    // return View::make('home.index');
-    return view('home.index');
+    function index()
+    {
+        $cars = Car::get();
+
+
+        //    return view('home.index', [ 'name' => 'John Doe', 'age' => 25 ]); 
+        // return View::make('home.index');
+        return view('home.index');
     }
 }
